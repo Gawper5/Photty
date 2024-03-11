250723e9e770ccf6808f5ec9c7fccf6a49959e91
Gašper <gawper.kumer5@gmail.com>
2024-03-11 17:26:28 +0100

    demo

---
+++ index.js
@@ -0,0 +1,51 @@
+const dropzone = document.getElementById('dropzone');
+const errorDisplay = document.getElementById('errorDisplay');
+let image;
+
+dropzone.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    dropzone.classList.remove("dragleave");
+    dropzone.classList.add("dragover");
+});
+
+dropzone.addEventListener("dragleave", (e) => {
+    e.preventDefault();
+    dropzone.classList.remove("dragover");
+    dropzone.classList.add("dragleave");
+});
+
+document.ondrop = e => e.preventDefault();
+document.ondragover = e => e.preventDefault();
+
+dropzone.addEventListener("drop", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    dropzone.classList.remove("dragover");
+    dropzone.classList.add("dragleave");
+    image = e.dataTransfer.files[0];
+    validate();
+});
+
+const inputElement = document.getElementById("files");
+inputElement.addEventListener("change", () => {
+    image = inputElement.files[0];
+    validate();
+});
+
+function validate(){
+    const allowedTypes = ['image/*'];
+    if (!allowedTypes.includes(image.type)) {
+        displayError("Only images are allowed.");
+        return;
+    }
+    clearError();
+    console.log(image);
+}
+
+function displayError(message) {
+    errorDisplay.textContent = message;
+}
+
+function clearError() {
+    errorDisplay.textContent = '';
+}
