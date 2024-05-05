b8bfe5acc66c19b1a1451d9c7cf38a8532065520
Gašper <gawper.kumer5@gmail.com>
2024-05-05 14:08:52 +0200

    finished

--- index.js
+++ index.js
@@ -714,7 +714,3 @@ function loadContent(page) {
 }
 
 loadContent('home');
-
-function map_range(value, low1, high1, low2, high2) {
-    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
-}
